09d6dfe25783bb8f7e0d688c1b9e4564d040f96d
VladislavAmbrosov <ambrosov413@gmail.com>
2026-04-04 19:35:45 +0300

    skills section

--- src/components/Wrapper/Container.tsx
+++ src/components/Wrapper/Container.tsx
@@ -2,7 +2,7 @@ import styled from "styled-components"
 
 
 export const Container = styled.div`
-    max-width: 1024px;
+    max-width: 1044px;
     margin: 0 auto;
     padding: 0 10px;
     min-height: 100%;
